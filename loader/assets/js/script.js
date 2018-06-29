9c0f3970c3dc41367b48a203b77c7134b514d30d
grozzzny <1234578>
2018-06-29 16:03:09 +0200

    loader

--- loader/assets/js/script.js
+++ loader/assets/js/script.js
@@ -1,6 +1,8 @@
 $(window).on('load', function (e) {
     //Hide prage loader
     $(".page-loader").fadeOut();
+});
+$(window).ready(function (e) {
     setTimeout(function () {
         $(".page-loader").fadeOut();
     }, 1500);
